afc2058d146eb01504b70c6af2bdc565f6e4514d
Ajay Quirk <ajay@quirky.codes>
2025-04-17 16:54:06 +1200

    tests: Basic test suites

--- src/main/java/co/nz/restorelab/smc/CalculateChangeProcess.java
+++ src/main/java/co/nz/restorelab/smc/CalculateChangeProcess.java
@@ -25,16 +25,26 @@ public class CalculateChangeProcess implements GeoServerProcess {
 
     @DescribeResult(name = "SMC Change", description = "Returns the change in soil moisture content.")
     public String execute(
-            @DescribeParameter(name = "Start Timestamp", description = "Starting timestamp for the calculation")
-            String startDate,
-            @DescribeParameter(name = "End Timestamp", description = "Ending timestamp for the calculation")
-            String endDate
+            @DescribeParameter(name = "Start Timestamp First", description = "Starting timestamp for the first flight.")
+            String startDateFirst,
+            @DescribeParameter(name = "End Timestamp First", description = "Ending timestamp for the first flight.")
+            String endDateFirst,
+            @DescribeParameter(name = "Start Timestamp Second", description = "Starting timestamp for the second flight.")
+            String startDateSecond,
+            @DescribeParameter(name = "End Timestamp Second", description = "Ending timestamp for the second flight.")
+            String endDateSecond
     ) throws Exception {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime startDateLocal = LocalDateTime.parse(startDate, formatter);
-        LocalDateTime endDateLocal = LocalDateTime.parse(endDate, formatter);
+        LocalDateTime startDateLocal = LocalDateTime.parse(startDateFirst, formatter);
+        LocalDateTime endDateLocal = LocalDateTime.parse(endDateFirst, formatter);
         if (!startDateLocal.isBefore(endDateLocal)) {
-            throw new IllegalArgumentException("End data must be after the start date!");
+            throw new IllegalArgumentException("The first flights end date must be after the first flights start date.");
+        }
+
+        LocalDateTime startDateSecondLocal = LocalDateTime.parse(startDateSecond, formatter);
+        LocalDateTime endDateSecondLocal = LocalDateTime.parse(endDateSecond, formatter);
+        if (!startDateSecondLocal.isBefore(endDateSecondLocal)) {
+            throw new IllegalArgumentException("The second flights end date must be after the second flights start date.");
         }
 
         FeatureTypeInfo featureType = catalog.getFeatureTypeByName("tiger:poly_landmarks");
@@ -48,8 +58,6 @@ public class CalculateChangeProcess implements GeoServerProcess {
             }
         }
 
-        System.out.println(featureSource);
-
-        return "Hello, this is testing" + startDate;
+        return "Hello, this is testing" + startDateFirst;
     }
 }
--- src/test/java/co/nz/restorelab/smc/CalculateChangeProcessTest.java
+++ src/test/java/co/nz/restorelab/smc/CalculateChangeProcessTest.java
@@ -1,68 +1,82 @@
-//package co.nz.restorelab.smc;
-//
-//import org.geoserver.catalog.Catalog;
-//import org.geoserver.catalog.FeatureTypeInfo;
-//import org.geotools.api.data.FeatureSource;
-//import org.geotools.api.data.SimpleFeatureSource;
-//import org.geotools.api.feature.simple.SimpleFeature;
-//import org.geotools.api.feature.simple.SimpleFeatureType;
-//import org.geotools.data.simple.SimpleFeatureCollection;
-//import org.geotools.feature.DefaultFeatureCollection;
-//import org.geotools.feature.simple.SimpleFeatureBuilder;
-//import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.time.format.DateTimeParseException;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.when;
-//
-//public class CalculateChangeProcessTest {
-//
-//    @Mock private Catalog catalog;
-//    @Mock private FeatureTypeInfo featureTypeInfo;
-//    @Mock private FeatureSource featureSource;
-//    @InjectMocks private CalculateChangeProcess process;
-//
-//    private SimpleFeatureCollection fakeFeatureCollection;
-//
-//    @BeforeEach
-//    void setUp() throws Exception {
-//        MockitoAnnotations.openMocks(this);
-//        process = new CalculateChangeProcess(catalog);
-//
-//        // Create fake feature type
-//        SimpleFeatureTypeBuilder typeBuilder = new SimpleFeatureTypeBuilder();
-//        typeBuilder.setName("TestType");
-//        typeBuilder.add("smc_average", Double.class);
-//        SimpleFeatureType featureType = typeBuilder.buildFeatureType();
-//
-//        // Fake features
-//        DefaultFeatureCollection collection = new DefaultFeatureCollection(null, featureType);
-//        SimpleFeatureBuilder builder = new SimpleFeatureBuilder(featureType);
-//        collection.add(builder.buildFeature(null, new Object[]{10.0}));
-//        collection.add(builder.buildFeature(null, new Object[]{20.0}));
-//        fakeFeatureCollection = collection;
-//
-//        when(catalog.getFeatureTypeByName("testLayer")).thenReturn(featureTypeInfo);
-//        when(featureTypeInfo.getFeatureSource(any(), any())).thenReturn(featureSource);
-//
-//    }
-//
-//    void runTestGood(String startDate, String endData) throws Exception {
-//        String result = process.execute(startDate, endData);
-//        assertEquals("Hello, this is testing2025-02-12 12:02:02", result);
-//    }
-//
-//    void runTestFail(String startDate, String endDate) {
-//        assertThrows(DateTimeParseException.class, () -> process.execute(startDate, endDate));
-//    }
-//
-//    @Test void testGoodDate() throws Exception {runTestGood("2025-02-12 12:02:02", "2025-02-13 12:02:02");}
-//    @Test void testBadDate() {runTestFail("2025 02 02 02:02:02", "2025 02 02 02:02:02");}
-//}
+package co.nz.restorelab.smc;
+
+import org.geoserver.catalog.Catalog;
+import org.geoserver.catalog.FeatureTypeInfo;
+import org.geotools.api.data.SimpleFeatureSource;
+import org.geotools.api.feature.simple.SimpleFeature;
+import org.geotools.api.feature.simple.SimpleFeatureType;
+
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.format.DateTimeParseException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.mockito.Mockito.*;
+
+public class CalculateChangeProcessTest {
+
+    private CalculateChangeProcess process;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        SimpleFeature feature = createFeature();
+        DefaultFeatureCollection test = new DefaultFeatureCollection();
+        test.add(feature);
+
+        SimpleFeatureSource mockFeatureSource = mock(SimpleFeatureSource.class);
+        when(mockFeatureSource.getFeatures()).thenReturn(test);
+
+        FeatureTypeInfo mockFeatureTypeInfo = mock(FeatureTypeInfo.class);
+        doReturn(mockFeatureSource).when(mockFeatureTypeInfo).getFeatureSource(null, null);
+
+        Catalog mockCatalog = mock(Catalog.class);
+        when(mockCatalog.getFeatureTypeByName("tiger:poly_landmarks")).thenReturn(mockFeatureTypeInfo);
+
+        process = new CalculateChangeProcess(mockCatalog);
+    }
+
+    @Test public void testGood() throws Exception {
+        String result = process.execute("2023-01-01 00:00:00", "2023-01-02 00:00:00",
+                "2024-01-01 00:00:00", "2024-01-02 00:00:00");
+        assertTrue(result.contains("Hello, this is testing"));
+    }
+
+    @Test public void testBadFirstDate() {
+        assertThrows(IllegalArgumentException.class, () -> process.execute("2024-01-01 00:00:00",
+                "2023-01-02 00:00:00",
+                "2024-01-01 00:00:00",
+                "2023-01-02 00:00:00"));
+    }
+
+    @Test public void testBadSecondDate() {
+        assertThrows(IllegalArgumentException.class, () -> process.execute("2023-01-01 00:00:00",
+                "2023-01-02 00:00:00",
+                "2024-01-01 00:00:00",
+                "2023-01-02 00:00:00"));
+    }
+
+    @Test public void testMalformedDate() {
+        assertThrows(DateTimeParseException.class, () -> process.execute("2023 01 01 00 00 00",
+                "2023-01-02 00:00:00",
+                "2024-01-01 00:00:00",
+                "2023-01-02 00:00:00"));
+    }
+
+    public SimpleFeature createFeature() {
+        SimpleFeatureTypeBuilder typeBuilder = new SimpleFeatureTypeBuilder();
+        typeBuilder.setName("poly_landmarks");
+        typeBuilder.add("LANAME", String.class);
+        SimpleFeatureType featureType = typeBuilder.buildFeatureType();
+
+        // Creating a fake feature
+        SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(featureType);
+        featureBuilder.add("Test County");
+        return featureBuilder.buildFeature("fid.1");
+    }
+}
